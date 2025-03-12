4f9c35d1c391673587db56cea7ece2747563bb76
Estela Fernández <alu0101560205@ull.edu.es>
2025-03-12 11:11:45 +0000

    Avances modi

---
+++ src/Modificacion/ComplexNumber.ts
@@ -0,0 +1,59 @@
+import { Arithmeticable } from "./ArithmeticableInterface";
+
+/**
+ * Clase para representar números complejos
+ */
+export class ComplexNumber implements Arithmeticable<ComplexNumber> {
+  private realPart: number;
+  private imaginaryPart: number;
+  /**
+   * Constructor de la clase COmplexNumber
+   * @param real parte real del número complejo
+   * @param imaginary parte imaginaria del numero complejo
+   */
+  constructor(real: number, imaginary: number) {
+    this.realPart = real;
+    this.imaginaryPart = imaginary;
+  }
+  /**
+   * Método para sumar dos números complejos
+   * @param otherNumber número que se pretende sumar al invocador
+   */
+  add(otherNumber: ComplexNumber): ComplexNumber {
+    let totalReal = this.realPart + otherNumber.realPart;
+    let totalImagianary = this.imaginaryPart + otherNumber.imaginaryPart;
+    let result = new ComplexNumber(totalReal, totalImagianary);
+    return result;
+  }
+  /**
+   * Método para restar dos números complejos
+   * @param otherNumber número que se le pretende restar al invocador
+   */
+  substract(otherNumber: ComplexNumber): ComplexNumber {
+    let totalReal = this.realPart - otherNumber.realPart;
+    let totalImagianary = this.imaginaryPart - otherNumber.imaginaryPart;
+    let result = new ComplexNumber(totalReal, totalImagianary);
+    return result;
+  }
+  /**
+   * Método para multiplicar dos números complejos
+   * @param otherNumber segundo número a multiplicar
+   */
+  multiply(otherNumber: ComplexNumber): ComplexNumber {
+    let totalReal = (this.realPart * otherNumber.realPart) - (this.imaginaryPart * otherNumber.imaginaryPart);
+    let totalImagianary = (this.realPart * otherNumber.imaginaryPart) + (this.imaginaryPart * otherNumber.realPart);
+    let result = new ComplexNumber(totalReal, totalImagianary);
+    return result;
+  }
+  /**
+   * Método para dividir dos números complejos
+   * @param otherNumber divisor utilizado en la división
+   */
+  divide(otherNumber: ComplexNumber): ComplexNumber {
+    let denominador = (otherNumber.realPart * otherNumber.realPart) + (otherNumber.imaginaryPart * otherNumber.imaginaryPart);
+    let totalReal = ((this.realPart * otherNumber.realPart) + (this.imaginaryPart * otherNumber.imaginaryPart)) / denominador;
+    let totalImagianary = ((this.imaginaryPart * otherNumber.realPart) - (this.realPart * otherNumber.imaginaryPart)) / denominador;
+    let result = new ComplexNumber(totalReal, totalImagianary);
+    return result;
+  }
+}
---
+++ tests/modificacion.spec.ts
@@ -0,0 +1,68 @@
+import { describe, expect, test, beforeEach } from "vitest";
+import { ComplexNumber } from "../src/Modificacion/ComplexNumber"
+import { Arithmeticable } from "../src/Modificacion/ArithmeticableInterface";
+import { ArithmeticableCollection } from "../src/Modificacion/ArithmeticableCollection"
+
+describe("Pruebas de ComplexNumbers", () => {
+  test("Instanciar elemento de la clase ComplexNumber", () => {
+    const complexNumber = new ComplexNumber(2, 3);
+    expect(complexNumber).toBeInstanceOf(ComplexNumber);
+  });
+
+  test("Debe sumar dos numeros complejos", () => {
+    const complexNumber = new ComplexNumber(2, 3);
+    const other = new ComplexNumber(1, 4);
+    const result = complexNumber.add(other);
+    let resultAdd = new ComplexNumber(3, 7);
+    expect(result).toEqual(resultAdd);
+  });
+
+  test("Debe sumar dos numeros complejos", () => {
+    const complexNumber = new ComplexNumber(6, 2);
+    const other = new ComplexNumber(3, 4);
+    const result = complexNumber.add(other);
+    let resultAdd = new ComplexNumber(9, 6);
+    expect(result).toEqual(resultAdd);
+  });
+
+  test("Debe restar dos numeros complejos", () => {
+    const complexNumber = new ComplexNumber(2, 3);
+    const other = new ComplexNumber(1, 4);
+    const result = complexNumber.substract(other);
+    let resultSub = new ComplexNumber(1, -1);
+    expect(result).toEqual(resultSub);
+  });
+
+  test("Debe multiplicar dos numeros complejos", () => {
+    const complexNumber = new ComplexNumber(2, 3);
+    const other = new ComplexNumber(1, 4);
+    const result = complexNumber.multiply(other);
+    let resultMul = new ComplexNumber(-10, 11)
+    expect(result).toEqual(resultMul);
+  });
+
+  test("Debe dividir dos numeros complejos", () => {
+    const complexNumber = new ComplexNumber(2, 4);
+    const other = new ComplexNumber(1, 3);
+    const result = complexNumber.divide(other);
+    let resultDiv = new ComplexNumber(14/10, -2/10);
+    expect(result).toEqual(resultDiv);
+  });
+
+  test("Debe dividir dos numeros complejos", () => {
+    const complexNumber = new ComplexNumber(3, 2);
+    const other = new ComplexNumber(5, 1);
+    const result = complexNumber.divide(other);
+    let resultDiv = new ComplexNumber(17/26, 7/26);
+    expect(result).toEqual(resultDiv);
+  });
+});
+
+describe("Pruebas de la clase ArithmeticableCollection", () => {
+  test("Crear añadir objeto a la colección", () => {
+    const colection = new ArithmeticableCollection<ComplexNumber>;
+    const complexNumber = new ComplexNumber(1, 2);
+    const prueba = colection.addArithmeticable(complexNumber);
+    expect(prueba).toBe([1,2]);
+  });
+});
